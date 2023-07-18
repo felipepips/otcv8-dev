5eccc7abe949a84cc9630ef6603809dea675fc0f
felipepips <42760405+felipepips@users.noreply.github.com>
2023-07-17 21:11:33 -0400

    lil mistake

--- modules/game_trainer/trainer.lua
+++ modules/game_trainer/trainer.lua
@@ -271,7 +271,8 @@ function mainLoop()
   local now = g_clock.seconds()
 
   -- anti idle
-  if set.checkIdle then
+  if set.checkIdle and (lastEat + config.turnInterval < now) then
+    lastTurn = now
     local dir = player:getDirection()
     dir = dir > 5 and 3 or (dir > 3 and 1 or dir)
     local newDir = dir
